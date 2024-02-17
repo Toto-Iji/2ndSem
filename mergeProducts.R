d0618adf164e3107671762dee791fce3bf2442d1
Aeron Jesse Edombingo <aeronjesse.edombingo@students.isatu.edu.ph>
2024-02-17 12:47:29 +0000

    Activity 3 and Organizing Mega Commit (Finished)

--- mergeProducts.R
+++
@@ -1,8 +0,0 @@
-readp1 <- read.csv("p1_final.csv")
-readp2 <- read.csv("p2_Final.csv")
-readp3 <- read.csv("p3_Final.csv")
-
-mergedf <- rbind(readp1, readp2, readp3)
-
-
-write.csv(mergedf, "ProductsFinal.csv", row.names = FALSE)
